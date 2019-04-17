cea25641874497f77d7944cf8e24edaf4b974320
Timo Lassmann <timo.lassmann@telethonkids.org.au>
2019-04-18 01:21:15 +0200

    added global header

---
+++ src/global.h
@@ -0,0 +1,17 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+#include "tldevel.h"
+
+
+#endif
---
+++ src/run_kalign.c
@@ -0,0 +1,9 @@
+#include "global.h"
+
+int main(int argc, char *argv[])
+{
+
+        return EXIT_SUCCESS;
+ERROR:
+        return EXIT_FAILURE;
+}
