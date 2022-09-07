686a71f5280220ed6a7f1ffe60a194c23b2d52d0
Timo Lassmann <timo.lassmann@telethonkids.org.au>
2022-09-07 14:46:08 +0800

    added cpp test

---
+++ tests/kalign_lib_testCXX.cpp
@@ -0,0 +1,46 @@
+
+#include <array>
+#include <iostream>
+#include <string>
+#include "kalign/kalign.h"
+
+int main() {
+        // Initialize array
+        char * inseq[95] = {
+                "GKGDPKKPRGKMSSYAFFVQTSREEHKKKHPDASVNFSEFSKKCSERWKTMSAKEKGKFEDMAKADKARYEREMKTYIPPKGE",
+                "MQDRVKRPMNAFIVWSRDQRRKMALENPRMRNSEISKQLGYQWKMLTEAEKWPFFQEAQKLQAMHREKYPNYKYRPRRKAKMLPK",
+                "MKKLKKHPDFPKKPLTPYFRFFMEKRAKYAKLHPEMSNLDLTKILSKKYKELPEKKKMKYIQDFQREKQEFERNLARFREDHPDLIQNAKK",
+                "MHIKKPLNAFMLYMKEMRANVVAESTLKESAAINQILGRRWHALSREEQAKYYELARKERQLHMQLYPGWSARDNYGKKKKRKREK",
+        };
+        int numseq = 4;
+
+        int* L = new int[numseq];        
+        L[0] = 83;
+        L[1] = 85;
+        L[2] = 91;
+        L[3] = 86;
+        
+        char** aln = NULL;
+        int aln_len = 0;
+
+                
+         kalign(inseq,L, numseq,4 , KALIGN_DNA_INTERNAL, -1, -1 , -1, &aln, &aln_len);
+        // foo(22);
+
+        // fprintf(stdout,"Aligned:\n");
+        std::cout << "Aligned:\n";
+        for(int i = 0; i < numseq;i++){
+                std::cout << aln[i] << "\n";
+                // fprintf(stdout,"%s\n", aln[i]);
+        }
+        /* Free alignment  */
+        for(int i = 0; i < numseq;i++){
+                // delete[] aln[i];
+                free(aln[i]);
+        }
+        free(aln);
+
+        
+
+        delete[] L;
+}
