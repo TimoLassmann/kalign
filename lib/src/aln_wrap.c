b4af7d27b85feb2fd7b4a3386c472d0080f25741
Timo Lassmann <timo.lassmann@telethonkids.org.au>
2022-09-08 16:47:24 +0800

    alpha

--- lib/src/aln_wrap.c
+++ lib/src/aln_wrap.c
@@ -24,7 +24,9 @@ int kalign(char **seq, int *len, int numseq,int n_threads, int type, float gpo, 
         struct msa *msa;
         RUN(kalign_arr_to_msa(seq, len,numseq, &msa));
         msa->quiet = 1;
-
+        if(n_threads < 1){
+                n_threads = 1;
+        }
         RUN(kalign_run(msa,n_threads, type,  gpo, gpe, tgpe));
         RUN(kalign_msa_to_arr(msa, aligned, out_aln_len));
         kalign_free_msa(msa);
